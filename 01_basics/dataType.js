77c796ab1491aa994642955726b874cd6293de17
Aman Kumar <93372156+amankr00@users.noreply.github.com>
2025-07-09 15:48:15 +0000

    datatypes

---
+++ 01_basics/dataType.js
@@ -0,0 +1,15 @@
+let name = "Aman kumar"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint (
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => Value is not assigned yet 
+// symbol => uniques,used in react for to identify different components
+
+
+console.log(typeof name)
