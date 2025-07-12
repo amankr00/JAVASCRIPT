d933a213dcf938e3ad2208ac77b6a4f894461f24
Aman Kumar <93372156+amankr00@users.noreply.github.com>
2025-07-12 18:19:57 +0000

    objects 1 done

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,22 +1,46 @@
 // Singleton 
 Object.create
+// Singleton
+const tinder = new Object()
+console.log(typeof(tinder))
 
 const mySym = '12y3'
 
 const jsUser = {
-    name : 'Aman',
-    [mySym] : '12345',
+    name: "Aman",
+    [mySym] : '12345', // Syntax for Symbal datatype
     age : 24,
     location : 'New York',
-    email : 'user2amankr@gmail.com',
-    isLoggedIn : false
+    email : 'amankr@gmail.com',
+    isLoggedIn : false,
+    lastLogin : ['monday' , 'friday']
 }
 
 /*
     Key is taken as strings by js , so accessing using 
-    jsUser.email is ok by right way is jsUser[email] as
+    jsUser.email is ok by right way is jsUser['email'] as
     js dosen't allow jsUser.'email' . 
 */
 
-console.log(jsUser[name])
+console.log(jsUser['name'])
+console.log(jsUser['email'])
 console.log(mySym)
+console.log(typeof([mySym]))
+
+jsUser.email = 'kr@reddit.com' // Overwrite
+console.log(jsUser['email'])
+
+// To make the object immutable , we also cannot add anything in the object 
+// Object.freeze(jsUser)
+
+jsUser.email = "xyz@ybl.com"  // Not updated as Object.freeze is used.
+console.log(jsUser['email']) 
+
+console.log(jsUser)
+
+jsUser.greeting = function(){
+    console.log('\nThis is a function')
+}
+
+jsUser.greeting()
+console.log(jsUser.greeting)
